fc597e9e983a9028fbc898d3f3ca02b28e530fef
surbhi <surbhigupta13jan@gmail.com>
2017-08-06 13:33:10 +0530

    changes

---
+++ src/app/home/deviceInfo/dropdown/dropdown.data.ts
@@ -0,0 +1,4 @@
+export const costCenter = [
+    {label:'Select Group', value:null},
+    {label:'Work performed', value:{id:1, name: 'New York', code: 'NY'}}
+]
--- src/app/home/deviceInfo/info/info.component.ts
+++ src/app/home/deviceInfo/info/info.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { costCenter } from '../dropdown/dropdown.data'
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.css']
 })
-export class InfoComponent {
+export class InfoComponent implements OnInit{
+  public dropdown = {
+    costCenter: costCenter
+  }
+  public info = {
+
+  }
   constructor(public router: Router) {}
-    
+  ngOnInit () {
+
+  }
+  
   
 }
