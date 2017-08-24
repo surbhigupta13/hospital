abbfa27f5ee0b8882998da993575b36119f67ea9
surbhi <surbhigupta13jan@gmail.com>
2017-08-25 01:45:38 +0530

    change

--- src/app/home/default/default.component.ts
+++ src/app/home/default/default.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-default',
   templateUrl: './default.component.html',
-  styleUrls: ['./default.component.scss']
+  styleUrls: ['./default.component.css']
 })
 export class DefaultComponent {
   data:any;
